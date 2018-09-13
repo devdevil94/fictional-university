f757e742ab15ed43dd5acbb6c781f35bfd671ed4
Saud <saudd2gh@gmail.com>
2018-09-13 11:01:04 +0800

    posts added to the blog

--- wp-content/themes/university-theme/index.php
+++ wp-content/themes/university-theme/index.php
@@ -11,7 +11,25 @@
   </div>
 
   <div class="container container--narrow page-section">
-  	Hello
+  	<?php 
+  		while (have_posts()) {
+  			the_post(); ?>
+			
+			<div class="post-item">
+				<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+				<div class="metabox">
+					<p>Posted by <?php the_author_posts_link(); ?> on 6.6.2017 in News</p>
+				</div>
+
+				<div class="generic-content">
+					<?php the_excerpt(); ?>
+					<p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Read More &raquo;</a></p>
+				</div>
+			</div>
+
+  		<?php }
+  		?>
   </div>
 
 <?php get_footer(); ?>
