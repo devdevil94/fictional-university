134978cce885e313b9e0eb4e10148e088aabae36
Saud <saudd2gh@gmail.com>
2018-10-15 20:50:30 +0800

    minor fixes

--- wp-content/themes/university-theme/archive-campus.php
+++ wp-content/themes/university-theme/archive-campus.php
@@ -14,11 +14,11 @@
         $mapLocation = get_field('map_location');
         ?>
   		 <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng'] ?>">
-         
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <?php echo $mapLocation['address'] ?>
        </div>
   		<?php 
       }
-  			echo paginate_links();
   	?>
     </div> 
   </div>
