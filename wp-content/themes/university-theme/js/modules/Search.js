410b0d48c174e4568b58272e36c5a8920074f259
Saud <saudd2gh@gmail.com>
2018-10-20 23:40:34 +0800

    using keybord events to open & close search overlay

--- wp-content/themes/university-theme/js/modules/Search.js
+++ wp-content/themes/university-theme/js/modules/Search.js
@@ -6,6 +6,7 @@ class Search{
 		this.openButton = $(".js-search-trigger");
 		this.closeButton = $(".search-overlay__close");
 		this.searchOverlay = $(".search-overlay");
+		this.isOverlayOpen = false;
 
 		this.events();
 	}
@@ -13,14 +14,29 @@ class Search{
 	events(){
 		this.openButton.on("click", this.openOverlay.bind(this));
 		this.closeButton.on("click", this.closeOverlay.bind(this));
+
+		$(document).on("keydown", this.keyPressDispatcher.bind(this));
 	}
 
 	openOverlay(){
 		this.searchOverlay.addClass("search-overlay--active");
+		this.isOverlayOpen = true;
+		$("body").addClass("body-no-scroll");
+
 	}
 
 	closeOverlay(){
 		this.searchOverlay.removeClass("search-overlay--active");
+		thi.isOverlayOpen = false;
+		$("body").removeClass("body-no-scroll");
+	}
+
+	keyPressDispatcher(e){
+		console.log(e.keyCode);
+		if(e.keyCode == 83 && !this.isOverlayOpen)
+			this.openOverlay();
+		else if (e.keyCode == 27 && this.isOverlayOpen)
+			this.closeOverlay();
 	}
 
 }
