3abdf9db54091920f432640e6c75e9d9b5d5a4b3
Saud <saudd2gh@gmail.com>
2018-10-26 17:45:55 +0800

    custom json data

--- wp-content/themes/university-theme/inc/search-route.php
+++ wp-content/themes/university-theme/inc/search-route.php
@@ -8,7 +8,22 @@
 	}
 
 	function uniSearchResults(){
-		return 'Route';
+		$professors = new WP_Query(array(
+				'post_type' => 'professor'
+		));
+
+		$profResults = array();
+
+		while ($professors->have_posts()) {
+			$professors->the_post();
+
+			array_push($profResults, array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink()
+			));
+		}
+
+		return $profResults;
 	}
 
 	add_action( 'rest_api_init', 'uniRegisterSearch');
