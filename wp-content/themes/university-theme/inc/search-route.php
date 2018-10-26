c4dc28a98f4bbd9fb628c2c4fcbe060190b90ca7
Saud <saudd2gh@gmail.com>
2018-10-26 17:45:36 +0800

    managing multiple types of search results

--- wp-content/themes/university-theme/inc/search-route.php
+++ wp-content/themes/university-theme/inc/search-route.php
@@ -8,23 +8,59 @@
 	}
 
 	function uniSearchResults($data){
-		$professors = new WP_Query(array(
-				'post_type' => 'professor',
+		$mainQuery = new WP_Query(array(
+				'post_type' => array('professor', 'post', 'page', 'program', 'campus', 'event'),
 				's' => sanitize_text_field($data['term'])
 		));
 
-		$profResults = array();
+		$results = array(
+			'generalInfo' => array(),
+			'professors' => array(),
+			'programs' => array(),
+			'events' => array(),
+			'campuses' => array()
+		);
+		
+		while ($mainQuery->have_posts()) {
+			$mainQuery->the_post();
 
-		while ($professors->have_posts()) {
-			$professors->the_post();
+			if(get_post_type() == 'post' OR get_post_type() == 'page'){
+				array_push($results['generalInfo'], array(
+					'title' => get_the_title(),
+					'permalink' => get_the_permalink()
+				));
+			}
 
-			array_push($profResults, array(
-				'title' => get_the_title(),
-				'permalink' => get_the_permalink()
-			));
+			if(get_post_type() == 'professor'){
+				array_push($results['professors'], array(
+					'title' => get_the_title(),
+					'permalink' => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == 'program'){
+				array_push($results['programs'], array(
+					'title' => get_the_title(),
+					'permalink' => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == 'event'){
+				array_push($results['events'], array(
+					'title' => get_the_title(),
+					'permalink' => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == 'campus'){
+				array_push($results['campuses'], array(
+					'title' => get_the_title(),
+					'permalink' => get_the_permalink()
+				));
+			}
 		}
 
-		return $profResults;
+		return $results;
 	}
 
 	add_action( 'rest_api_init', 'uniRegisterSearch');
