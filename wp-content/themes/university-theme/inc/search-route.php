e46d35c49cb96f81b69cba0f94d8b0ec8760c847
Saud <saudd2gh@gmail.com>
2018-11-01 19:47:19 +0800

    program-professor meta query added

--- wp-content/themes/university-theme/inc/search-route.php
+++ wp-content/themes/university-theme/inc/search-route.php
@@ -44,7 +44,8 @@
 			if(get_post_type() == 'program'){
 				array_push($results['programs'], array(
 					'title' => get_the_title(),
-					'permalink' => get_the_permalink()
+					'permalink' => get_the_permalink(),
+					'id' => get_the_id()
 				));
 			}
 
@@ -73,31 +74,38 @@
 			}
 		}
 
-		$programRelationshipQuery = new WP_Query(array(
-			'post_type' => 'professor',
-			'meta_query' => array(
-				array(
-					'key' => 'related_program',
-					'compare' => 'LIKE',
-					'value' => '"52"'
-				)
-			) 
-		));
+		if($results['programs']){
 
-		while($programRelationshipQuery->have_posts()){
-			$programRelationshipQuery->the_post();
-
-			if(get_post_type() == 'professor'){
-				array_push($results['professors'], array(
-					'title' => get_the_title(),
-					'permalink' => get_the_permalink(),
-					'img' => get_the_post_thumbnail_url(0,'profLandscape')
-				));
+			$programsMetaQuery = array('relation' => 'OR');
+			foreach($results['programs'] as $item){
+				array_push($programsMetaQuery, array(
+						'key' => 'related_programs',
+						'compare' => 'LIKE',
+						'value' => '"'.$item['id'].'"'
+					)
+				);
 			}
+
+			$programRelationshipQuery = new WP_Query(array(
+				'post_type' => 'professor',
+				'meta_query' => $programsMetaQuery
+			));
+
+			while($programRelationshipQuery->have_posts()){
+				$programRelationshipQuery->the_post();
+
+				if(get_post_type() == 'professor'){
+					array_push($results['professors'], array(
+						'title' => get_the_title(),
+						'permalink' => get_the_permalink(),
+						'img' => get_the_post_thumbnail_url(0,'profLandscape')
+					));
+				}
+			}
+
+			$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
 		}
-
-		$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
-
+		
 		return $results;
 	}
 
